0c45a123597a72c2640d38f7cc9e5eb45080e4e1
Yancharuk Alexander <alex@itvault.info>
2016-09-18 08:42:54 +0300

    Small code cleanup

--- public/app/view/main/MainController.js
+++ public/app/view/main/MainController.js
@@ -7,15 +7,5 @@
 Ext.define('Artics.view.main.MainController', {
     extend: 'Ext.app.ViewController',
 
-    alias: 'controller.main',
-
-    //onItemSelected: function (sender, record) {
-    //    Ext.Msg.confirm('Confirm', 'Are you sure?', 'onConfirm', this);
-    //},
-    //
-    //onConfirm: function (choice) {
-    //    if (choice === 'yes') {
-    //        //
-    //    }
-    //}
+    alias: 'controller.main'
 });
